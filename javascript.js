75c22d6e31a8d3b9fe30f5f73aa2881db62594ab
Vijay Saharan <105264602+vijaysaharan@users.noreply.github.com>
2022-08-21 09:10:51 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let btn=document.getElementById('subs');
+btn.addEventListener('click',function1);
+function function1(){
+    alert('Now, You are a new Subscriber!!!!');
+}
+let btn1=document.getElementById('sum');
+btn.addEventListener('click',function2);
+function function2(){
+    alert('Your form submitted!!!');
+}
